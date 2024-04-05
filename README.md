12759881aebf5cda1f6163f5deb864eaf2f1bf9f
Timothy Flynn <trflynn89@pm.me>
2024-04-05 13:47:50 -0400

    SSE app

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # sse-demo
+
+Setup:
+
+```bash
+virtualenv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+Running:
+```
+flask run
+```
+
+Then navigate to http://127.0.0.1:5000 in your browser of choice.
---
+++ app.py
@@ -0,0 +1,50 @@
+import time
+
+from datetime import datetime
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+HTML = """
+<body>
+<pre id="contents"></pre>
+<script>
+    let contents = document.querySelector("#contents");
+    let sse = new EventSource("/sse");
+
+    const println = (string) => {
+        const text = document.createTextNode(`${string}\\n`);
+        contents.appendChild(text);
+    };
+
+    sse.onopen = () => {
+        println("SSE connection open");
+    };
+
+    sse.onmessage = (event) => {
+        println(`SSE: ${event.data}`);
+    };
+
+    sse.onerror = () => {
+        println("SSE connection error");
+    };
+</script>
+</body>
+"""
+
+
+@app.route('/')
+def root():
+    return Response(HTML, mimetype='text/html')
+
+
+@app.route('/sse')
+def sse():
+    def generate():
+        while True:
+            now = datetime.now().ctime()
+            yield "data: {}\n\n".format(now)
+
+            time.sleep(3)
+
+    return Response(generate(), mimetype='text/event-stream')
